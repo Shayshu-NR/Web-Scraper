a653725e3f5c31e20445cefddd44dbcd5d8fca50
Shayshu-NR <shayshu.nahata@mail.utoronto.ca>
2021-08-03 23:24:53 -0400

    Initial commit

---
+++ default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="default.aspx.cs" Inherits="WebScraper._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Web Scraper</title>
+    <link rel="icon" href="code_black_24dp.svg" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div id="scrapeUI" runat="server">
+
+    </div>
+</body>
+
+<script>
+    $(document).ready(function () {
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+
+        $(".btn.btn-primary").on("click", function () {
+            // Make sure both the url and path are valid, if no path is specified then use ./
+            var url = $("#requestURL").val();
+            var path = $("#downloadLocation").val();
+            var dataDict = JSON.stringify({ "requestInfo": [url, path] } );
+            console.log(dataDict);
+
+            $.ajax({
+                type: "POST",
+                url: "default.aspx/download",
+                dataType: "json",
+                data: dataDict,
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    
+                    var recvData = JSON.parse(data.d);
+                    console.log(data.d);
+                    console.log(recvData);
+                }
+            });
+        })
+    })
+</script>
+</html>
---
+++ default.aspx.cs
@@ -0,0 +1,132 @@
+﻿using Nager.PublicSuffix;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+
+namespace WebScraper
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Either read the query string or make a UI...
+
+            try
+            {
+                string requestURL = HttpUtility.UrlDecode(Request.QueryString["link"]);
+                string saveLocation = HttpUtility.UrlDecode(Request.QueryString["path"]);
+                string webPage = getPage(requestURL);
+                savePage(webPage, saveLocation, requestURL);
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+            StringBuilder webScraperUI = new StringBuilder();
+
+            webScraperUI.Append(@"
+                <br>
+                <div class=""container"">
+                   <div class=""row justify-content-center"">
+                      <div class=""col-auto"">
+                         <div class=""card"">
+                            <div class=""card-body"">
+                               <h3 class=""card-title text-center"">Web Scraper Tool</h3>
+                               <div class=""input-group mb-3"">
+                                  <input type=""text"" id=""requestURL"" class=""form-control"" aria-label=""Request URL"" placeholder=""www.example.com"" data-toggle=""tooltip"" data-placement=""auto"" title=""Website to be scraped"">
+                                  <input type=""text"" id=""downloadLocation"" class=""form-control"" aria-label=""Save Location"" placeholder=""C:/Users/Downloads"" data-toggle=""tooltip"" data-placement=""auto"" title=""Where to Save website"">
+                               </div>
+                               <div class=""row text-center"">
+                                  <div class=""col-md-12"">
+                                    <button type = ""button"" class=""btn btn-primary"">Get Web Page</button>
+                                  </div>
+                               </div>
+                            </div>
+                         </div>
+                      </div>
+                   </div>
+                </div>");
+
+            scrapeUI.InnerHtml = webScraperUI.ToString();
+
+        }
+
+        public static string getPage(string requestURL)
+        {
+            try
+            {
+
+                if (!string.IsNullOrEmpty(requestURL))
+                {
+                    // Make a quick get request
+                    Uri uriResult;
+                    bool validURL = Uri.TryCreate(requestURL, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+                    if (validURL)
+                    {
+                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestURL);
+                        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                        StringBuilder webPage = new StringBuilder();
+
+                        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                        using (Stream stream = response.GetResponseStream())
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            webPage.Append(reader.ReadToEnd());
+                        }
+
+                        return webPage.ToString();
+                    }
+                }
+                return "Failed to get web page";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public static bool savePage(string webPage, string saveLocation, string requestURL)
+        {
+            if (!string.IsNullOrEmpty(saveLocation))
+            {
+                try
+                {
+                    // Save the data to the specified path...
+                    var domainParser = new DomainParser(new WebTldRuleProvider());
+                    var domainInfo = domainParser.Parse(requestURL);
+
+                    File.WriteAllText(saveLocation + domainInfo.RegistrableDomain + ".html", webPage.ToString());
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        [WebMethod]
+        public static string download(List<string> requestInfo)
+        {
+            if (requestInfo.Count > 1)
+            {
+                string requestURL = requestInfo[0];
+                string saveLocation = requestInfo[1];
+                string webPage = getPage(requestURL);
+                return savePage(webPage, saveLocation, requestURL) ? "{ \"Success\" : \"Web Page Saved Successfully\" }" : "{ \"Error\" : \"Failed To Save Web Page\" }";
+            }
+            else
+            {
+                return "{ \"Error\" : \"Not Enough Arguments Supplied\" }";
+            }
+        }
+    }
+
+}
